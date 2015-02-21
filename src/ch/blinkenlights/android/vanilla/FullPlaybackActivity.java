2d91cac8dcd9b4aff1e43e744bcf0097a179e77c
Adrian Ulrich <adrian@blinkenlights.ch>
2015-02-21 14:38:14 +0100

    holo bringup

--- src/ch/blinkenlights/android/vanilla/FullPlaybackActivity.java
+++ src/ch/blinkenlights/android/vanilla/FullPlaybackActivity.java
@@ -252,8 +252,6 @@ public class FullPlaybackActivity extends PlaybackActivity
 	{
 		if (mOverlayText == null) {
 			TextView view = new TextView(this);
-			view.setBackgroundColor(Color.BLACK);
-			view.setTextColor(Color.WHITE);
 			view.setGravity(Gravity.CENTER);
 			view.setPadding(25, 25, 25, 25);
 			// Make the view clickable so it eats touch events
